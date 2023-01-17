bdeaa7a6b529959fc1851041644d816ebd56e194
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-01-17 11:57:43 +0100

    Ajout du style pour les textview des noms de joueurs

--- app/src/main/java/com/renachl/speedquiz/StartActivity.java
+++ app/src/main/java/com/renachl/speedquiz/StartActivity.java
@@ -2,13 +2,35 @@ package com.renachl.speedquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button BT_Jouer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        BT_Jouer = findViewById(R.id.start_bt_jouer);
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        BT_Jouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent quizActivity = new Intent(StartActivity.this, QuizActivity.class);
+                startActivity(quizActivity);
+            }
+        });
+    }
+
 }

9401f691798f6d9b1ac16c35154355ed48a91523
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-01-20 14:56:40 +0100

    Changement des id sur les Edit Text dans l'xml

--- app/src/main/java/com/renachl/speedquiz/StartActivity.java
+++ app/src/main/java/com/renachl/speedquiz/StartActivity.java
@@ -6,18 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
 
     private Button BT_Jouer;
+    private Button BT_Annuler;
+    private EditText ET_SaisiJ1;
+    private EditText ET_SaisiJ2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        //Récupération des composants
         BT_Jouer = findViewById(R.id.start_bt_jouer);
-
+        BT_Annuler = findViewById(R.id.start_bt_annuler);
+        ET_SaisiJ1 = findViewById(R.id.start_et_nom_j1);
+        ET_SaisiJ2 = findViewById(R.id.start_et_nom_j2);
     }
 
     @Override
@@ -31,6 +39,15 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(quizActivity);
             }
         });
+
+        BT_Annuler.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ET_SaisiJ1.setText("");
+                ET_SaisiJ2.setText("");
+            }
+        });
+
     }
 
 }
